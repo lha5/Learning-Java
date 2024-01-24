41e663d8e6a1c3d458f92cbb8d86be6c78bbe01c
lha <hyeahnlee5@gmail.com>
2024-01-24 21:20:59 +0900

    HelloJava

---
+++ src/HelloJava.java
@@ -0,0 +1,12 @@
+public class HelloJava {
+    // HelloJava를 클래스(class)라 한다.
+    // 파일명과 클래스 이름이 같아야 한다.
+    // {} 블록을 사용해서 클래스의 시작과 끝을 나타낸다.
+    public static void main(String[] args) {
+        // main 메서드라 한다.
+        // 자바는 main(String[] args) 메서드를 찾아서 프로그램을 시작한다.
+        // main(String[] args)은 프로그램의 시작점이라고 이해할 수 있다.
+        // {} 블록을 사용해서 메서드의 시작과 끝을 나타낸다.
+        System.out.println("hello java");
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+        // 프로그램은 main(String[] args)을 시작으로 위에서 차례로 한 줄씩 실행된다.
+    }
+}
