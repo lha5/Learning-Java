3239d587e949fc913fe522402d21bd77340b6272
lha <hyeahnlee5@gmail.com>
2024-02-07 22:58:08 +0900

    Condition - else if

--- src/condition/If4.java
+++ src/condition/If4.java
@@ -1,28 +1,19 @@
 package condition;
 
-public class If3 {
+public class If4 {
 
     public static void main(String[] args) {
         int age = 14;
 
         if (age <= 7) {
-            // ~7: 미취학
             System.out.println("미취학");
-        }
-        if (age >= 8 && age <= 13) {
-            // 8~13: 초등학생
+        } else if (age <= 13) {
             System.out.println("초등학생");
-        }
-        if (age >= 14 && age <= 16) {
-            // 14~16: 중학생
+        } else if (age <= 16) {
             System.out.println("중학생");
-        }
-        if (age >= 17 && age <= 19) {
-            // 17~19: 고등학생
+        } else if (age <= 19) {
             System.out.println("고등학생");
-        }
-        if (age >= 20) {
-            // 20~: 성인
+        } else {
             System.out.println("성인");
         }
     }
