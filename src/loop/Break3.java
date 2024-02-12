b17a7d4b0bcb7952f169884977863749470946e3
lha <hyeahnlee5@gmail.com>
2024-02-12 12:08:24 +0900

    Loop - for and nested

--- src/loop/Break3.java
+++ src/loop/Break3.java
@@ -1,18 +1,16 @@
 package loop;
 
-public class Break2 {
+public class Break3 {
 
     public static void main(String[] args) {
         int sum = 0;
-        int i = 1;
 
-        for (; ; ) {
+        for (int i = 1; ; i++) {
             sum += i;
             if (sum > 10) {
                 System.out.println("합이 10보다 크면 종료: i = " + i + ", sum = " + sum);
                 break;
             }
-            i++;
         }
     }
 }
